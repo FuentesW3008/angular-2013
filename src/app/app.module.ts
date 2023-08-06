aac4ec8f487b46af041ece1606672bf90cde4000
Alfonso Fuentes <fuentesw3008@gmail.com>
2023-08-06 17:20:08 -0500

    FinalDBZ

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,10 +1,13 @@
 import { NgModule } from '@angular/core';
 
 import { BrowserModule } from '@angular/platform-browser';
+import { DbzModule } from './dbz/dbz.module';
 import { HeroesModule } from './heroes/heroes.module';
 
 import { AppComponent } from './app.component';
 import { CounterComponent } from './counter/counter.component';
+
+
 
 
 
@@ -15,7 +18,8 @@ import { CounterComponent } from './counter/counter.component';
   ],
   imports: [
     BrowserModule,
-    HeroesModule
+    DbzModule,
+    HeroesModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
